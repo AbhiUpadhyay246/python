456d83c049069ae6e56031e72d6d738de85d8391
AbhiUpadhyay246 <upadhyayabhi339@gmail.com>
2026-02-03 19:22:26 +0530

    Create problem2.py

---
+++ chapter2/problem2.py
@@ -0,0 +1,19 @@
+# Arthimatic opeartor 
+a = 3
+b = 6
+sum = a + b
+print(sum)
+
+# Assingment opeartor
+a = 4-2 # assign in a
+b = 6
+b += 3
+print(b)
+
+#comparison operator 
+c = a<b
+print(c)
+
+# logical operators
+e = True or False
+print(e) 
