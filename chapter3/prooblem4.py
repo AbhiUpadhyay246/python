267d41ec02546ff5a75c5c1db378781403759449
AbhiUpadhyay246 <upadhyayabhi339@gmail.com>
2026-02-05 08:47:29 +0530

    Create prooblem4.py to illustrate string replacement
    Add a script to demonstrate string immutability in Python.

---
+++ chapter3/prooblem4.py
@@ -0,0 +1,5 @@
+string = " gokul is a good  boy"
+print(string.replace("  "," "))
+print(string) 
+"""string are immutable which means 
+              that you cannot change then by running function on them"""
