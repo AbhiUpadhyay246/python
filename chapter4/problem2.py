aeb24da8571120a0936c907828715276c353df2d
AbhiUpadhyay246 <upadhyayabhi339@gmail.com>
2026-03-23 08:52:41 +0530

    Add script to input and sort marks

---
+++ chapter4/problem2.py
@@ -0,0 +1,20 @@
+
+marks = []
+f1 = int(input("enter a marks number ="))
+marks.append(f1)
+f2 = int(input("enter a marks number ="))
+marks.append(f2)
+f3 = int(input("enter a marks number ="))
+marks.append(f3)
+f4 = int(input("enter a marks number ="))
+marks.append(f4)
+f5 = int(input("enter a marks number ="))
+marks.append(f5)
+print(marks)
+marks.sort()
+print(marks)
+
+
+
+
+
